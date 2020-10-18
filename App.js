52ec6c0905be8cce8f2ab29935ff32b6777da0a1
Henrique Assis <henrique_assis_10@hotmail.com>
2020-10-18 20:23:51 -0300

    adding libraries

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Helllo</Text>
       <StatusBar style="auto" />
     </View>
   );
