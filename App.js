2002f22494efc50ebe29b65bf302ddcbc15a7a3e
Henrique Assis <henrique_assis_10@hotmail.com>
2020-10-19 08:59:47 -0300

    create context

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+
+import UserContextProvider from './src/contexts/UserContext';
 
 import MainStack from './src/stacks/MainStack';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    <UserContextProvider>
+        <NavigationContainer>
+          <MainStack />
+        </NavigationContainer>
+    </UserContextProvider>
+
   );
 }
 
