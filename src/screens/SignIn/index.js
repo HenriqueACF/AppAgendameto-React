cbb5c04455a73bef81cc35a10e30e88dcc8e17d1
Henrique Assis <henrique_assis_10@hotmail.com>
2020-10-19 09:49:27 -0300

    screen login

--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Text } from 'react-native';
-import {Container} from './styles';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+//components
+import SignInput from '../../components/SignInput';
+
+//svg
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
 
 export default () =>{
   return (
     <Container>
-      <Text>SignIN</Text>
+      <BarberLogo width="100%" height="160"/>
+
+      <InputArea>
+        
+        <SignInput 
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail" 
+        />
+
+        <SignInput 
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+        />
+
+        <CustomButton>
+            <CustomButtonText>Login</CustomButtonText>
+        </CustomButton>
+
+      </InputArea>
+
+      <SignMessageButton>
+          <SignMessageButtonText>Ainda nao possui uma conta?</SignMessageButtonText>
+          <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
+
     </Container>
   )
 }
