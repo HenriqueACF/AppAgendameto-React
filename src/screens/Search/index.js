4b7459822011c5ee506611febed23b47d6faeaf4
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-03 11:08:22 -0300

    search screen

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -3,14 +3,44 @@ import {
     Container,
     SearchArea,
     SearchInput,
+    Scroller,
+    LoadingIcon,
+    ListArea,
+    EmptyWarning
 } from './styles';
+
+//api
+import Api from '../../Api';
+
+//components
+import BarberItem from '../../components/BarberItem';
 
 export default () =>{
 
     const [searchText, setSearchText] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [emptyList, setEmptyList] = useState(false);
+    const [list, setList] = useState([])
 
-    const searchBarbers = () =>{
+    const searchBarbers = async () =>{
+        setEmptyList(false);
+        setLoading(true);
+        setList([]);
 
+        if(searchText != ''){
+            let res = await Api.search(searchText);
+            if(res.error == ''){
+                if(res.list.length > 0){
+                    setList(res.list);
+                }else{
+                    setEmptyList(true);
+                }
+            }else{
+                alert('Error: ' + res.error);
+            }
+        }
+
+        setLoading(false);
     }
 
     return (
@@ -27,6 +57,23 @@ export default () =>{
                     selectTextOnFocus
                 />
             </SearchArea>
+
+            <Scroller>
+                {loading &&
+                    <LoadingIcon size="large" color="#000" />
+                }
+
+                {emptyList &&
+                    <EmptyWarning>'Não encontramos nenhum: '${searchText}</EmptyWarning>
+                }
+
+                <ListArea>
+                    {list.map((item, k)=>(
+                        <BarberItem key={k} data={item} />
+                    ))}
+                </ListArea>
+            </Scroller>
+
         </Container>    
     );
 }
--- src/screens/Search/styles.js
+++ src/screens/Search/styles.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView `
@@ -12,10 +11,32 @@ export const SearchArea = styled.View `
     border-radius:20px;
     padding:0 20px;
     margin:20px;
+    margin-bottom:0px;
 `;
 
 export const SearchInput = styled.TextInput `
     flex:1;
     font-size:16px;
     color:#FFF;
+`;
+
+export const Scroller = styled.ScrollView`
+    flex:1;
+    padding:0 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator `
+    margin-top:30px;
+`;
+
+export const ListArea = styled.View `
+    margin-top:20px;
+    margin-bottom:20px;
+`;
+
+export const EmptyWarning = styled.Text `
+    text-align:center;
+    margin-top:30px;
+    color:#FFF;
+    font-size:14px;
 `;
