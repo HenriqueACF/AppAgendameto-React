e876de4642c75d83f86a5566ba2b56205bd0862e
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-04 14:22:43 -0300

    initi appointments

--- React Native/src/screens/Appointments/index.js
+++ React Native/src/screens/Appointments/index.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, {useState, useEffect} from 'react';
+import {RefreshControl} from 'react-native';
+import { 
+    Container,
+    Scroller,
+    ListArea,
+    EmptyWarning
+} from './styles';
+
+//api
+import Api from '../../Api';
+
+//components
+import AppointmentItem from '../../components/AppointmentItem';
 
 export default () =>{
+
+    const [loading, setLoading] = useState(false);
+    const [list, setList] = useState([]);
+
+    useEffect(()=>{
+        getAppointments();   
+    }, [])
+
+    const getAppointments = async () =>{
+        setLoading(true);
+        setList([]);
+
+        let res = await Api.getAppointments();
+        if(res.error == ''){
+            if(res.list.length > 0){
+                setList(res.list);
+            }else{
+                alert('Error: ' + res.error);
+        }
+
+        setLoading(false);
+    }
+
     return (
         <Container>
-            <Text>Appointments</Text>
+
+            <Scroller  refreshControl={
+                    <RefreshControl refreshing={loading} onRefresh={getAppointments} /> 
+            }>
+
+            {!loading && list.length === 0 &&
+                <EmptyWarning>Não há agendamentos.</EmptyWarning>
+            }
+
+                <ListArea>
+                    {list.map((item, k)=>(
+                        <AppointmentItem key={k} data={item} />
+                    ))}
+                </ListArea>
+            </Scroller>
+
         </Container>    
     );
+    }
 }
